9ba62d49917ff3fcb65baa9c08a16cd243204551
AFNasser <AhmadF.Nasser@gmail.com>
2025-01-04 18:24:06 +0400

    CircleShape and Player classes created, player drawn on screen, Clock object added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -9,8 +10,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # pygame initialization
     pygame.init()
+    dt = 0  # delta Time
+    timer = pygame.time.Clock() # time tracking object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # player initialization
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +27,9 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = timer.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
